9940d4b6967bd6d89f04f9698d4e99af6449997f
alkampfer <ricci.gianmaria@gmail.com>
2014-10-20 18:33:17 +0200

    First structure, still not working.

--- src/Jarvis.ConfigurationService.Host/Support/ConfigurationServiceApplication.cs
+++ src/Jarvis.ConfigurationService.Host/Support/ConfigurationServiceApplication.cs
@@ -4,6 +4,10 @@ using System.Web.Http;
 using Microsoft.Owin;
 using Owin;
 using System.Net.Http.Headers;
+using System.IO;
+using System.Linq;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
 
 [assembly: OwinStartup(typeof(Jarvis.ConfigurationService.Host.Support.ConfigurationServiceApplication))]
 
@@ -14,6 +18,7 @@ namespace Jarvis.ConfigurationService.Host.Support
         public void Configuration(IAppBuilder app)
         {
             ConfigureWebApi(app);
+            ConfigureAdmin(app);
         }
 
         protected virtual void ConfigureWebApi(IAppBuilder appBuilder)
@@ -25,11 +30,11 @@ namespace Jarvis.ConfigurationService.Host.Support
             // Web API routes
             config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-                );
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //    );
 
             //Force always returning json
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(
@@ -37,6 +42,42 @@ namespace Jarvis.ConfigurationService.Host.Support
                 );
 
             appBuilder.UseWebApi(config);
+        }
+
+        void ConfigureAdmin(IAppBuilder application)
+        {
+            var root = AppDomain.CurrentDomain.BaseDirectory
+                .ToLowerInvariant()
+                .Split(Path.DirectorySeparatorChar)
+                .ToList();
+
+            while (true)
+            {
+                var last = root.Last();
+                if (last == String.Empty || last == "debug" || last == "release" || last == "bin")
+                {
+                    root.RemoveAt(root.Count - 1);
+                    continue;
+                }
+
+                break;
+            }
+
+            root.Add("app");
+
+            var appFolder = String.Join("" + Path.DirectorySeparatorChar, root);
+
+            var fileSystem = new PhysicalFileSystem(appFolder);
+
+            var options = new FileServerOptions
+            {
+                EnableDirectoryBrowsing = true,
+                FileSystem = fileSystem,
+                EnableDefaultFiles = true
+            };
+
+            application.UseFileServer(options);
+
         }
     }
 }
